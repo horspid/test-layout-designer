aa9e546a2d07e3d5fa391c3b1522663020459a56
Maksim Gudovshchikov <horspid@yandex.ru>
2025-05-16 13:38:13 +0500

    fixed adaptive

--- scripts/index.js
+++ scripts/index.js
@@ -15,9 +15,11 @@ popupCloseButton.addEventListener("click", hidePopup);
 const menu = document.getElementById("header");
 const menuSwitchButton = document.getElementById("header-burger");
 const menuList = document.getElementById("header-nav__list");
+const content = document.getElementById("content");
 
 function menuSwitch() {
   menu.classList.toggle("header--mobile");
   menuSwitchButton.classList.toggle("header-burger--close");
   menuList.classList.toggle("header-nav__list--mobile");
+  content.classList.toggle("content--mobile");
 }
--- scripts/slider.js
+++ scripts/slider.js
@@ -1,14 +1,26 @@
-window.addEventListener("load", () => {
-  new Snake(".slider", {
-    infinite: false,
-    slidesToShow: 1,
-    speed: 300,
-    dots: false,
-    arrows: true,
-    prevArrow:
-      '<button type="button"><img src="img/icons/slider-arrow.svg" /><span>loren ipsum</span></button>',
-    nextArrow:
-      '<button type="button"><span>loren ipsum</span><img src="img/icons/slider-arrow.svg" /></button>',
-    mobileFirst: true,
-  });
-});
+window.addEventListener("load", () => {});
+
+let snakeSlider;
+
+function initSnakeSlider() {
+  if (window.innerWidth <= 1000 && !snakeSlider) {
+    snakeSlider = new Snake(".slider", {
+      infinite: false,
+      slidesToShow: 1,
+      speed: 300,
+      dots: false,
+      arrows: true,
+      prevArrow:
+        '<button type="button"><img src="img/icons/slider-arrow.svg" /><span>loren ipsum</span></button>',
+      nextArrow:
+        '<button type="button"><span>loren ipsum</span><img src="img/icons/slider-arrow.svg" /></button>',
+      mobileFirst: true,
+    });
+  } else if (window.innerWidth > 1000 && snakeSlider) {
+    snakeSlider.destroy();
+    snakeSlider = null;
+  }
+}
+
+window.addEventListener("load", initSnakeSlider);
+window.addEventListener("resize", initSnakeSlider);
